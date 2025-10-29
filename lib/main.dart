9cf81ef447f418364c4d2c555bb6cb49bf035579
Nguyen Duong Ngoc Anh <23011500@st.phenikaa-uni.edu.vn>
2025-10-29 05:52:03 +0000

    new

--- lib/main.dart
+++ lib/main.dart
@@ -7,3 +7,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   runApp(const LanguageFlashcardsApp());
 }
+// =================== APP ===================
+
+class LanguageFlashcardsApp extends StatelessWidget {
+  const LanguageFlashcardsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Language Flashcards',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: const SplashOrLogin(),
+    );
+  }
+}
